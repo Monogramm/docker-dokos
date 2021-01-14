1af6e6f818678d88dd80a4948e728603f582da34
christophe.romain <christophe.romain@monogramm.io>
2021-01-14 16:02:48 +0100

    :bug: Force use of dodock python env

--- template/bin/install_app.sh
+++ template/bin/install_app.sh
@@ -6,6 +6,7 @@ APP_BRANCH=${3}
 
 cd /home/dodock/dodock-bench/
 
+. env/bin/activate
 echo -e "frappe\n${APP_NAME}" > sites/apps.txt
 
 cd ./apps
@@ -13,11 +14,13 @@ cd ./apps
 git clone --depth 1 ${BRANCH} ${APP_REPO} ${APP_NAME}
 
 cd ${APP_NAME}
+sed -i -e '/frappe/d' requirements.txt
 yarn
 rm -rf node_modules
 yarn install --production=true
 yarn add node-sass
 
 cd /home/dodock/dodock-bench/
+pip3 install --no-cache-dir -e apps/${APP_NAME}
 mkdir -p sites/assets/${APP_NAME}
 cp -R apps/${APP_NAME}/${APP_NAME}/public/* sites/assets/${APP_NAME}
