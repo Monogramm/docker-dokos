5870c4b361c9c381a0a40eb5c8796a7fe636f0c7
christophe.romain <christophe.romain@monogramm.io>
2021-01-15 15:30:21 +0100

    :art: Cleanup install_app

--- template/bin/install_app.sh
+++ template/bin/install_app.sh
@@ -7,20 +7,20 @@ APP_BRANCH=${3}
 cd /home/dodock/dodock-bench/
 
 . env/bin/activate
-echo -e "frappe\n${APP_NAME}" > sites/apps.txt
 
+echo "${APP_NAME}" >> sites/apps.txt
 cd ./apps
 [ "${APP_BRANCH}" ] && BRANCH="-b ${APP_BRANCH}"
 git clone --depth 1 ${BRANCH} ${APP_REPO} ${APP_NAME}
+sed -i -e '/frappe/d' ${APP_NAME}/requirements.txt
+pip3 install --no-cache-dir -e /home/dodock/dodock-bench/apps/${APP_NAME}
 
 cd ${APP_NAME}
-sed -i -e '/frappe/d' requirements.txt
 yarn
 rm -rf node_modules
 yarn install --production=true
 yarn add node-sass
 
 cd /home/dodock/dodock-bench/
-pip3 install --no-cache-dir -e apps/${APP_NAME}
 mkdir -p sites/assets/${APP_NAME}
 cp -R apps/${APP_NAME}/${APP_NAME}/public/* sites/assets/${APP_NAME}
